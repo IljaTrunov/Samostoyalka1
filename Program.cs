e29f6739a62776f36324047c5b4bc88acbd37122
Opilane TTHK <opilane@tthk.ee>
2022-02-28 10:52:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samostoyalka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите строку: ");
+            //string str = Console.ReadLine();
+            //Console.WriteLine("Введите символ: ");
+            //char sum = Console.ReadKey().KeyChar;
+            //Console.WriteLine();
+
+            //int length = str.Length;
+            //char[] strArr = str.ToCharArray();
+            //int counter = 0;
+            //for (int i = 0; i < length; i++)
+            //{
+            //    if (strArr[i] == sum)
+            //    {
+            //        counter++;
+            //    }
+            //}
+            //double lengthDouble = Convert.ToDouble(length);
+            //double counterDouble = Convert.ToDouble(counter);
+            //double perc = (counterDouble / lengthDouble) * 100;
+            //Console.WriteLine($"Процент вхождения символа {sum} равен {perc}%");
+
+            //Console.Write("N  = ");
+            //var n = int.Parse(Console.ReadLine());
+            //var a = new int[n];
+            //var r = new Random();
+            //var count = 0;
+            //for (var i = 0; i < n; i++)
+            //{
+            //    a[i] = r.Next(-300, 300);
+            //    if (a[i] > -100 && a[i] < 100) count++;
+            //}
+            //Console.WriteLine("Count {0}", count);
+
+            //Console.WriteLine("Напиши свою зарплату -->");
+            //int ZP = Console.ReadLine();
+
+            //if ZP <= 550
+
+        }
+    }
+}
